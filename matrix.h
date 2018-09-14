99d35914593100bf75fd9cad785b83ad4302b564
senyacherenkov <senyacherenkov@yandex.ru>
2018-09-14 21:15:43 +0300

    new version

--- matrix.h
+++ matrix.h
@@ -4,35 +4,80 @@
 #include <tuple>
 #include <climits>
 
-class Matrix;
+#include <map>
+#include <cassert>
+#include <iterator>
+#include <functional>
 
-template <typename T, typename U>
-class Representation {
+namespace  {
+    using IncrementCallback = std::function<void()>;
+}
+template<typename T, T DEFAULT_VALUE>
+class Element {
+
 public:
-    U default_value;
-    Representation(Matrix& matrix):
-        m_matrix(matrix)
+    Element():
+        m_callback(nullptr)
     {}
 
-    T& operator=(T value){
-        if(value != default_value)
-            notify();
-        return m_value;
+    Element& operator= (const T& value) {
+        m_baseElement = value;
+        if(m_baseElement != DEFAULT_VALUE && !m_callback)
+            m_callback();
     }
 
-    void notify();
+    void setCallback(IncrementCallback callback) { m_callback = callback; }
 private:
-    T m_value = default_value;
-    Matrix m_matrix;
+    T m_baseElement;
+    IncrementCallback m_callback;
 };
 
-template<typename T, typename U>
-class Matrix {
-private:
+template <typename T, T DEFAULT_VALUE>
+class Row
+{
+public:
+    Row():
+        m_callback(nullptr)
+    {}
 
-    Representation<T,N>& operator[](int n) { return m_representation;}
+    T& operator[] (const T& column)
+    {
+        if(m_ElementBuff.find(column) == m_ElementBuff.end()) {
+            m_ElementBuff[column] = DEFAULT_VALUE;
+            if(!m_callback)
+                m_ElementBuff[column].setCallback(m_callback);
+        }
+        return m_ElementBuff[column];
+    }
+
+    void setCallback(IncrementCallback callback) { m_callback = callback; }
 private:
-    Representation<T,N>     m_representation;
-    std::size_t             m_size;
+    std::map<std::int32_t, Element<T, DEFAULT_VALUE>> m_ElementBuff;
+    IncrementCallback m_callback;
 };
 
+
+template <typename T, T DEFAULT_VALUE>
+class Matrix
+{
+public:
+    Matrix():
+        m_incrementSize(incrementSize)
+    {}
+
+    auto& operator[] (const T& row)
+    {
+        auto& tempRow = m_RawBuff[row];
+        tempRow.setCallback(m_incrementSize);
+        return tempRow;
+    }
+
+    std::size_t size() const { return m_size; }
+
+    void incrementSize() { m_size++;}
+
+private:
+    std::map<std::int32_t, Row<T,DEFAULT_VALUE>> m_RawBuff;
+    std::size_t m_size;
+    IncrementCallback m_incrementSize;
+};
